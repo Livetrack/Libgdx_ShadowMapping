9e4c1f650497e4e2c22f5092ceece15c1d5b3f09
Hossein ABBAS <gogolive20@gmail.com>
2019-07-11 19:51:35 +0200

    Firs commit of Libgdx Shadow Mapping

---
+++ core/src/com/shadowmapping/DirectionalLight.java
@@ -0,0 +1,13 @@
+package com.shadowmapping;
+
+import com.badlogic.gdx.math.Vector3;
+
+public class DirectionalLight {
+	public Vector3 direction;
+	public Vector3 color;
+	
+	public DirectionalLight(Vector3 direction, Vector3 color) {
+		this.direction = direction;
+		this.color = color;
+	}
+}
